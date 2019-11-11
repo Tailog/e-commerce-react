297c57d2bb62c8976a8e3be2eefef914403b0e8d
Santos Pierre <santospierreemmanuel@gmail.com>
2019-11-11 15:24:33 +0100

    Sign Up Component

--- src/components/Pages/signIn-signUp/SignInPage.jsx
+++ src/components/Pages/signIn-signUp/SignInPage.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 import './signinpage.scss';
 
 import SignIn from './../../signIn/SignIn';
+import SignUp from './../../signup/SignUp';
 
 const SignInPage = ()=>{
   return (
-    <div className="sign-in-sign-up">
+    <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
-  )
+  );
 }
 
 export default SignInPage;

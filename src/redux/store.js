650557ade97969d5078e0ac6a084703fe7505809
Santos Pierre <santospierreemmanuel@gmail.com>
2019-11-26 19:41:32 +0100

    Persist Redux/ Local Storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import {createStore, applyMiddleware} from "redux";
+import {persistStore} from 'redux-persist';
 import logger from 'redux-logger';
 
 import mainReducer from './mainReducer';
 
 const middlewares = [logger];
 
-const store = createStore(mainReducer,applyMiddleware(...middlewares));
+export const store = createStore(mainReducer,applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {store,persistor};
